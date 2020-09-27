20fa8a612bac0cc79ef149e11ca395b5732ff00f
markevillive <mark_evillive@hotmail.com>
2020-09-27 19:12:05 +0700

    lesson 3.1.0

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
 echo "<h1>МОЙ ПЕРВЫЙ БЛОГ</h1>";
+//echo time();
+function add($param1, $param2){
+    return $param1 + $param2;
+}
+$a = $_GET["a"];
+$b = $_GET["b"];
+
+//echo "a + b = ".($a + $b);
+echo add($a,$b)."<br>";
+
+for ($i =0;$i<10;$i++){
+    echo $i."<br>";
+}
+
 ?>
