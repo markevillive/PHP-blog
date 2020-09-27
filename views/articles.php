f369a787d1e2ab37b95d2e1fc069700bd10dc3f8
markevillive <mark_evillive@hotmail.com>
2020-09-28 00:43:55 +0700

    lesson 5.1.0

---
+++ views/articles.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Мой первый блог </title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="CSS/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <h1 class="navbar-brand">Мой первый блог.</h1>
+    </nav>
+
+    
+    <div class="article">
+        <h2></h2>
+        <em></em>
+        <p></p>
+    </div><!-----------------------------END ARTICLE----------------------------------->
+
+    <footer>
+        <h6>Мой первый блог</h6>
+        <p>&copy; Marc Shreder 2020.</p>
+    </footer>
+</div><!-----------------------------------------------------END CONTAINER------------------------------------------------------------->
+
+</html>
